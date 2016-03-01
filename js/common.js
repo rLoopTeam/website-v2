09ab2561c68f7cc5b47bbdfadf88f63043dea981
Paul <paullehenaff@msn.com>
2016-03-01 12:44:43 +0000

    Added scroll functionality to down arrow

--- js/common.js
+++ js/common.js
@@ -3,3 +3,52 @@ function toggleHamburgerMenu(){
   var navbar = document.querySelector('.navbar')
   navbar.classList.toggle('mobile-nav');
 }
+// easing function http://goo.gl/5HLl8
+Math.easeInOutQuad = function (t, b, c, d) {
+	t /= d/2;
+	if (t < 1) {
+		return c/2*t*t + b
+	}
+	t--;
+	return -c/2 * (t*(t-2) - 1) + b;
+};
+
+window.onload = function() {
+	var frontpageButton = document.querySelector("#front-down-page-arrow");
+	frontpageButton.addEventListener("click",runScroll,false);
+	
+	function runScroll() {
+		var pageHeight 	= window.innerHeight;
+		var newPos 		= document.documentElement.scrollTop || document.body.parentNode.scrollTop || document.body.scrollTop;
+		var startPos 	= newPos;
+
+		var step 		= 0;
+    	var start    	= startPos;
+    	var change		= pageHeight - newPos;
+    	var duration 	= 180;
+
+		var timeout;
+		var amount;
+		
+		animate();
+
+		function animate(){
+            if (step >= duration)
+            	return;
+
+            setTimeout(function(){
+
+		    	amount = Math.easeInOutQuad(step, start, change, duration);
+
+		    	document.documentElement.scrollTop = amount;
+			    document.body.parentNode.scrollTop = amount;
+			    document.body.scrollTop = amount;
+
+            	animate();
+
+            }, 1);
+
+			step++;
+		}
+	}
+}
